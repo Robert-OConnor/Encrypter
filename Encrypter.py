8cef3a0db716a7b1c223707c941a6c728060abcd
Robert OConnor <roboconn@uat.edu>
2020-03-11 22:38:07 -0700

    Fixed menu to allow argument parsing

--- Encrypter.py
+++ Encrypter.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 
-import cryptography
 from cryptography.fernet import Fernet
 from cryptography.hazmat.primitives import hashes
 from cryptography.hazmat.primitives.asymmetric import padding
@@ -9,7 +8,6 @@ from cryptography.hazmat.primitives.asymmetric import rsa
 from cryptography.hazmat.primitives import serialization
 import argparse
 import hashlib
-import os.path
 from os import path
 import sys
 
@@ -25,13 +23,12 @@ print('''
                          ''')
 
 print('==================================================')
-print('               Welcome to Encrypter.')
+print('               Welcome to Encrypter')
 print('==================================================')
-print("\n1: Symmetric file encryption.\n2: Asymmetric file encryption.\n3: MD5 hash generation.")
-print("4: SHA1 hash generation.\n5: SHA256 hash generation.\n6: SHA512 hash generation.\n")
+print('        Use -h to list possible arguments')
 
 #Encrypting the file
-def symmetric_encryption():
+def symmetric_encryption(option):
     #Create encryption key and store it in a file
     encryptionKey = Fernet.generate_key()
     file = open('key.key', 'wb')
@@ -40,7 +37,7 @@ def symmetric_encryption():
     print("[+] Stored encryption key in key.key\n")
 
     #Asks for file location and verifies if it exists
-    input_file = input("Enter input file for symmetric encryption.")
+    input_file = option
     if path.exists(input_file):
         print("[+] File exists")
     else:
@@ -64,7 +61,7 @@ def symmetric_encryption():
         print("[+] Finished Encrypting File")
 
 
-def asymmetric_encryption():
+def asymmetric_encryption(option):
     #Generates an RSA key pair
     private_key = rsa.generate_private_key(
         public_exponent=65537,
@@ -94,7 +91,7 @@ def asymmetric_encryption():
         f.write(pem)
 
     #Asks for the input file to be encrypted
-    input_file = input("Enter input file for asymmetric encryption.")
+    input_file = option
     #Verifies if the file exists
     if path.exists(input_file):
         print("[+] File exists")
@@ -122,8 +119,8 @@ def asymmetric_encryption():
 
 
 #MD5 Hash of File
-def MD5_hash():
-    input_file = input("Enter file to generate MD5 Hash: ")
+def MD5_hash(option):
+    input_file = option
     if path.exists(input_file):
         print("[+] File exists")
     else:
@@ -140,8 +137,8 @@ def MD5_hash():
     print('This is the MD5 hash of the ',input_file, 'file:\n' ,hasher.hexdigest()) #Get the hexadecimal digest of the hash
 
 #SHA1 Hash based encryption
-def SHA1_hash():
-    input_file = input("Enter file to generate SHA1 Hash: ")
+def SHA1_hash(option):
+    input_file = option
     if path.exists(input_file):
         print("[+] File exists")
     else:
@@ -158,8 +155,8 @@ def SHA1_hash():
     print('This is the SHA1 hash of the ',input_file, 'file:\n' ,hasher.hexdigest()) #Get the hexadecimal digest of the hash
 
 #SHA256 Hash based encryption
-def SHA256_hash():
-    input_file = input("Enter file to generate SHA256 Hash: ")
+def SHA256_hash(option):
+    input_file = option
     if path.exists(input_file):
         print("[+] File exists")
     else:
@@ -176,8 +173,8 @@ def SHA256_hash():
     print('This is the SHA256 hash of the ',input_file, 'file:\n' ,hasher.hexdigest()) #Get the hexadecimal digest of the hash
 
 #SHA512 Hash based encryption
-def SHA512_hash():
-    input_file = input("Enter file to generate SHA512 Hash: ")
+def SHA512_hash(option):
+    input_file = option
     if path.exists(input_file):
         print("[+] File exists")
     else:
@@ -193,47 +190,13 @@ def SHA512_hash():
     print('\n[+] Hash has been generated')
     print('This is the SHA512 hash of the ',input_file, 'file:\n' ,hasher.hexdigest()) #Get the hexadecimal digest of the hash
 
-#parser = argparse.ArgumentParser(description='Hashing and File Encryption')
+parser = argparse.ArgumentParser()
 
-#parser.add_argument("-s", "--symmetric", help="Use symmetric file encryption.")
-#parser.add_argument("-a", "--asymmetric", help="Use asymmetric file encryption.")
-#parser.add_argument("-m", "--md5", help="Path of file to generate MD5 hash.")
-#parser.add_argument("-s1", "--sha1", help="Path of file to generate SHA1 hash.")
-#parser.add_argument("-s256", "--sha256", help="Path of file to generate SHA256 hash.")
-#parser.add_argument("-s512", "--sha512", help="Path of file to generate SHA512 hash.")
+parser.add_argument("--sym", type=symmetric_encryption, action="store", help="File input for symmetric encryption")
+parser.add_argument("--asym", type=MD5_hash, action="store", help="File input for asymmetric encryption.")
+parser.add_argument("--md5", type=MD5_hash, action="store", help="Generates MD5 hash of file.")
+parser.add_argument("--sha1", type=SHA1_hash, action="store", help="Generates SHA1 hash of file.")
+parser.add_argument("--sha256", type=SHA256_hash, action="store", help="Generates SHA256 hash of file.")
+parser.add_argument("--sha512", type=SHA512_hash, action="store", help="Generates SHA512 hash of file.")
 
-#args = parser.parse_args()
-
-#if args.s == None or args.a == None or args.md5 == None or args.sha1 == None or args.sha256 == None or args.sha512 == None:
-#    print("Missing arguments")
-#else:
-#    if args.s:
-#        symmetric_encryption()
-#    if args.a:
-#        asymmetric_encryption()
-#    if args.md5:
-#        MD5_hash()
-#    if args.sha1:
-#        SHA1_hash()
-#    if args.sha256:
-#        SHA256_hash()
-#    if args.sha512:
-#        SHA512_hash()
-
-choice = input("\nSelect option: ")
-
-if choice == '1':
-    symmetric_encryption()
-elif choice == '2':
-    asymmetric_encryption()
-elif choice == '3':
-    MD5_hash()
-elif choice == '4':
-    SHA1_hash()
-elif choice == '5':
-    SHA256_hash()
-elif choice == '6':
-    SHA512_hash()
-else:
-    print("Error processing choice.")
-    sys.exit(0)
+args = parser.parse_args()
