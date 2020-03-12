9430688b7e8e7d711eccfcb3e92c5a69a799db56
Robert OConnor <roboconn@uat.edu>
2020-03-12 13:19:43 -0700

    Added SHA3-256 and SHA3-512 hash. Changed menu to be in __main__ and updated comments

--- Encrypter.py
+++ Encrypter.py
@@ -11,21 +11,6 @@ import hashlib
 from os import path
 import sys
 
-print('''
-  ______                             _
- |  ____|                           | |
- | |__   _ __   ___ _ __ _   _ _ __ | |_ ___ _ __
- |  __| | '_ \ / __| '__| | | | '_ \| __/ _ \ '__|
- | |____| | | | (__| |  | |_| | |_) | ||  __/ |
- |______|_| |_|\___|_|   \__, | .__/ \__\___|_|
-                          __/ | |
-                         |___/|_|
-                         ''')
-
-print('==================================================')
-print('               Welcome to Encrypter')
-print('==================================================')
-print('        Use -h to list possible arguments')
 
 #Encrypting the file
 def symmetric_encryption(option):
@@ -37,8 +22,7 @@ def symmetric_encryption(option):
     print("[+] Stored encryption key in key.key\n")
 
     #Asks for file location and verifies if it exists
-    input_file = option
-    if path.exists(input_file):
+    if path.exists(option):
         print("[+] File exists")
     else:
         print("[+] File does not exist")
@@ -48,10 +32,10 @@ def symmetric_encryption(option):
     output_file = input("Enter output file to store encrypted data: ")
 
     #Reads input file and stored content into data
-    with open(input_file, 'rb') as f:
+    with open(option, 'rb') as f:
         data = f.read()
 
-    #Encrypts data
+    #Encrypts data using fernet key
     fernet = Fernet(encryptionKey)    
     encrypted = fernet.encrypt(data)
 
@@ -91,15 +75,14 @@ def asymmetric_encryption(option):
         f.write(pem)
 
     #Asks for the input file to be encrypted
-    input_file = option
     #Verifies if the file exists
-    if path.exists(input_file):
+    if path.exists(option):
         print("[+] File exists")
     else:
         print("[+] File does not exist")
         sys.exit(0)
 
-    with open(input_file, 'rb') as f:
+    with open(option, 'rb') as f:
         data = f.read()
 
     encrypted = public_key.encrypt(
@@ -120,83 +103,218 @@ def asymmetric_encryption(option):
 
 #MD5 Hash of File
 def MD5_hash(option):
-    input_file = option
-    if path.exists(input_file):
+    #Verifies if inputted file exists
+    if path.exists(option):
         print("[+] File exists")
     else:
         print("[+] File does not exist")
         sys.exit(0)
-    BLOCKSIZE = 65536 #The size of each read from the file
-    hasher = hashlib.md5() #Create the hash object in MD5
-    with open(input_file, 'rb') as afile: #Open the file to read it's bytes
-        buf = afile.read(BLOCKSIZE) #Read from the file. Take in the BLOCKSIZE amount
-        while len(buf) > 0: #While there is still data being read from the file
-            hasher.update(buf) #Update the hash
-            buf = afile.read(BLOCKSIZE) #Read the next block from the file
-    print('\n[+] Hash has been generated')
-    print('This is the MD5 hash of the ',input_file, 'file:\n' ,hasher.hexdigest()) #Get the hexadecimal digest of the hash
+    #The size of each read from the file
+    BLOCKSIZE = 65536
+    #Create the hash object in MD5
+    hasher = hashlib.md5()
+    #Open the file to read it's bytes
+    with open(option, 'rb') as afile:
+        #Read from the file. Take in the BLOCKSIZE amount
+        buf = afile.read(BLOCKSIZE)
+        #While there is still data being read from the file
+        while len(buf) > 0:
+            #Update the hash
+            hasher.update(buf)
+            #Read the next block from the file
+            buf = afile.read(BLOCKSIZE)
+    print("\n[+] Hash has been generated")
+    #Get the hexadecimal digest of the hash
+    print("\nMD5 hash of ",option, ":\n" ,hasher.hexdigest(), sep="")
 
-#SHA1 Hash based encryption
+#SHA1 Hash of File
 def SHA1_hash(option):
-    input_file = option
-    if path.exists(input_file):
+    #Verifies if inputted file exists
+    if path.exists(option):
         print("[+] File exists")
     else:
         print("[+] File does not exist")
         sys.exit(0)
-    BLOCKSIZE = 65536 #The size of each read from the file
-    hasher = hashlib.sha1() #Create the hash object in SHA1
-    with open(input_file, 'rb') as afile: #Open the file to read it's bytes
-        buf = afile.read(BLOCKSIZE) #Read from the file. Take in the BLOCKSIZE amount
-        while len(buf) > 0: #While there is still data being read from the file
-            hasher.update(buf) #Update the hash
-            buf = afile.read(BLOCKSIZE) #Read the next block from the file
-    print('\n[+] Hash has been generated')
-    print('This is the SHA1 hash of the ',input_file, 'file:\n' ,hasher.hexdigest()) #Get the hexadecimal digest of the hash
+    #The size of each read from the file
+    BLOCKSIZE = 65536
+    #Create the hash object in SHA1
+    hasher = hashlib.sha1()
+    #Open the file to read it's bytes
+    with open(option, 'rb') as afile:
+        #Read from the file. Take in the BLOCKSIZE amount
+        buf = afile.read(BLOCKSIZE)
+        #While there is still data being read from the file
+        while len(buf) > 0:
+            #Update the hash
+            hasher.update(buf)
+            #Read the next block from the file
+            buf = afile.read(BLOCKSIZE)
+    print("\n[+] Hash has been generated")
+    #Get the hexadecimal digest of the hash
+    print("\nSHA1 hash of ",option, ":\n" ,hasher.hexdigest(), sep="")
 
-#SHA256 Hash based encryption
+#SHA256 Hash of File
 def SHA256_hash(option):
-    input_file = option
-    if path.exists(input_file):
+    #Verifies if inputted file exists
+    if path.exists(option):
         print("[+] File exists")
     else:
         print("[+] File does not exist")
         sys.exit(0)
-    BLOCKSIZE = 65536 #The size of each read from the file
-    hasher = hashlib.sha256() #Create the hash object in SHA256
-    with open(input_file, 'rb') as afile: #Open the file to read it's bytes
-        buf = afile.read(BLOCKSIZE) #Read from the file. Take in the BLOCKSIZE amount
-        while len(buf) > 0: #While there is still data being read from the file
-            hasher.update(buf) #Update the hash
-            buf = afile.read(BLOCKSIZE) #Read the next block from the file
+    #The size of each read from the file
+    BLOCKSIZE = 65536
+    #Create the hash object in SHA256
+    hasher = hashlib.sha256()
+    #Open the file to read it's bytes
+    with open(option, 'rb') as afile:
+        #Read from the file. Take in the BLOCKSIZE amount
+        buf = afile.read(BLOCKSIZE)
+        #While there is still data being read from the file
+        while len(buf) > 0:
+            #Update the hash
+            hasher.update(buf)
+            #Read the next block from the file
+            buf = afile.read(BLOCKSIZE)
     print('\n[+] Hash has been generated')
-    print('This is the SHA256 hash of the ',input_file, 'file:\n' ,hasher.hexdigest()) #Get the hexadecimal digest of the hash
+    #Get the hexadecimal digest of the hash
+    print("\nSHA256 hash of ",option, ":\n" ,hasher.hexdigest(), sep="")
 
-#SHA512 Hash based encryption
+#SHA384 Hash of File
+def SHA384_hash(option):
+    #Verifies if inputted file exists
+    if path.exists(option):
+        print("[+] File exists")
+    else:
+        print("[+] File does not exist")
+        sys.exit(0)
+    #The size of each read from the file
+    BLOCKSIZE = 65536
+    #Create the hash object in SHA384
+    hasher = hashlib.sha384()
+    #Open the file to read it's bytes
+    with open(option, 'rb') as afile:
+        #Read from the file. Take in the BLOCKSIZE amount
+        buf = afile.read(BLOCKSIZE)
+        #While there is still data being read from the file
+        while len(buf) > 0:
+            #Update the hash
+            hasher.update(buf)
+            #Read the next block from the file
+            buf = afile.read(BLOCKSIZE)
+    print("\n[+] Hash has been generated")
+    #Get the hexadecimal digest of the hash
+    print("\nSHA384 hash of ",option, ":\n" ,hasher.hexdigest(), sep="")
+
+#SHA512 Hash of File
 def SHA512_hash(option):
-    input_file = option
-    if path.exists(input_file):
+    #Verifies if inputted file exists
+    if path.exists(option):
         print("[+] File exists")
     else:
         print("[+] File does not exist")
         sys.exit(0)
-    BLOCKSIZE = 65536 #The size of each read from the file
-    hasher = hashlib.sha512() #Create the hash object in SHA512
-    with open(input_file, 'rb') as afile: #Open the file to read it's bytes
-        buf = afile.read(BLOCKSIZE) #Read from the file. Take in the BLOCKSIZE amount
-        while len(buf) > 0: #While there is still data being read from the file
-            hasher.update(buf) #Update the hash
-            buf = afile.read(BLOCKSIZE) #Read the next block from the file
-    print('\n[+] Hash has been generated')
-    print('This is the SHA512 hash of the ',input_file, 'file:\n' ,hasher.hexdigest()) #Get the hexadecimal digest of the hash
+    #The size of each read from the file
+    BLOCKSIZE = 65536
+    #Create the hash object in SHA512
+    hasher = hashlib.sha512()
+    #Open the file to read it's bytes
+    with open(option, 'rb') as afile:
+        #Read from the file. Take in the BLOCKSIZE amount
+        buf = afile.read(BLOCKSIZE)
+        #While there is still data being read from the file
+        while len(buf) > 0:
+            #Update the hash
+            hasher.update(buf)
+            #Read the next block from the file
+            buf = afile.read(BLOCKSIZE)
+    print("\n[+] Hash has been generated")
+    #Get the hexadecimal digest of the hash
+    print("\nSHA512 hash of ",option, ":\n" ,hasher.hexdigest(), sep="")
 
-parser = argparse.ArgumentParser()
+#SHA3_256 Hash of File
+def SHA3_256_hash(option):
+    #Verifies if inputted file exists
+    if path.exists(option):
+        print("[+] File exists")
+    else:
+        print("[+] File does not exist")
+        sys.exit(0)
+    #The size of each read from the file
+    BLOCKSIZE = 65536
+    #Create the hash object in SHA3-256
+    hasher = hashlib.sha3_256()
+    #Open the file to read it's bytes
+    with open(option, 'rb') as afile:
+        #Read from the file. Take in the BLOCKSIZE amount
+        buf = afile.read(BLOCKSIZE)
+        #While there is still data being read from the file
+        while len(buf) > 0:
+            #Update the hash
+            hasher.update(buf)
+            #Read the next block from the file
+            buf = afile.read(BLOCKSIZE)
+    print("\n[+] Hash has been generated")
+    #Get the hexadecimal digest of the hash
+    print("\nSHA3-256 hash of ",option, ":\n" ,hasher.hexdigest(), sep="")
 
-parser.add_argument("--sym", type=symmetric_encryption, action="store", help="File input for symmetric encryption")
-parser.add_argument("--asym", type=MD5_hash, action="store", help="File input for asymmetric encryption.")
-parser.add_argument("--md5", type=MD5_hash, action="store", help="Generates MD5 hash of file.")
-parser.add_argument("--sha1", type=SHA1_hash, action="store", help="Generates SHA1 hash of file.")
-parser.add_argument("--sha256", type=SHA256_hash, action="store", help="Generates SHA256 hash of file.")
-parser.add_argument("--sha512", type=SHA512_hash, action="store", help="Generates SHA512 hash of file.")
+#SHA3_512 Hash of File
+def SHA3_512_hash(option):
+    #Verifies if inputted file exists
+    if path.exists(option):
+        print("[+] File exists")
+    else:
+        print("[+] File does not exist")
+        sys.exit(0)
+    #The size of each read from the file
+    BLOCKSIZE = 65536
+    #Create the hash object in SHA3-512
+    hasher = hashlib.sha3_512()
+    #Open the file to read it's bytes
+    with open(option, 'rb') as afile:
+        #Read from the file. Take in the BLOCKSIZE amount
+        buf = afile.read(BLOCKSIZE)
+        #While there is still data being read from the file
+        while len(buf) > 0:
+            #Update the hash
+            hasher.update(buf)
+            #Read the next block from the file
+            buf = afile.read(BLOCKSIZE)
+    print("\n[+] Hash has been generated")
+    #Get the hexadecimal digest of the hash
+    print("\nSHA3-512 hash of ",option, ":\n" ,hasher.hexdigest(), sep="")
 
-args = parser.parse_args()
+
+def main():
+
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument("--sym", type=symmetric_encryption, action="store", help="File input for symmetric encryption")
+    parser.add_argument("--asym", type=asymmetric_encryption, action="store", help="File input for asymmetric encryption.")
+    parser.add_argument("--md5", type=MD5_hash, action="store", help="Generates MD5 hash of file.")
+    parser.add_argument("--sha1", type=SHA1_hash, action="store", help="Generates SHA1 hash of file.")
+    parser.add_argument("--sha256", type=SHA256_hash, action="store", help="Generates SHA256 hash of file.")
+    parser.add_argument("--sha384", type=SHA384_hash, action="store", help="Generates SHA384 hash of file.")
+    parser.add_argument("--sha512", type=SHA512_hash, action="store", help="Generates SHA512 hash of file.")
+    parser.add_argument("--sha3_256", type=SHA3_256_hash, action="store", help="Generates SHA3-256 hash of file.")
+    parser.add_argument("--sha3_512", type=SHA3_512_hash, action="store", help="Generates SHA3-512 hash of file.")
+
+    parser.parse_args()
+
+if __name__ == "__main__":
+    menu = """
+    ______                             _
+    |  ____|                           | |
+    | |__   _ __   ___ _ __ _   _ _ __ | |_ ___ _ __
+    |  __| | '_ \ / __| '__| | | | '_ \| __/ _ \ '__|
+    | |____| | | | (__| |  | |_| | |_) | ||  __/ |
+    |______|_| |_|\___|_|   \__, | .__/ \__\___|_|
+                            __/ | |
+                            |___/|_|
+    """
+    print(menu)
+    print('==================================================')
+    print('               Welcome to Encrypter')
+    print('==================================================')
+    print('        Use -h to list possible arguments')
+    
+    main()
